c456907cd1e682769403528a13d09d72ec1ccce6
18497038 <2>
2021-03-10 18:18:38 +0300

    добавил парсинг json body + изменил рестовую точку

--- src/main/java/com/example/demo/AiaController.java
+++ src/main/java/com/example/demo/AiaController.java
@@ -1,15 +1,23 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class AiaController {
 
-    @PostMapping("/send")
-    public String send() {
-        System.out.println("Получен запрос");
+    @RequestMapping(value = "/aia-webhook", method = POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String aiaWebhook(HttpServletRequest request) throws IOException {
+        final String json = IOUtils.toString(request.getInputStream(), String.valueOf(StandardCharsets.UTF_8));
+        System.out.println("json = " + json);
         return "Получен запрос";
     }
 
